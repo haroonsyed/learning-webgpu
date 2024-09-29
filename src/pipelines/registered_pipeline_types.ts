7d24494daf83e313e9c165613e88bb73dbd6ee26
haroonsyed <haroonaminsyed@gmail.com>
2024-09-29 04:48:22 -0400

    Added loading scene from json

---
+++ src/pipelines/registered_pipeline_types.ts
@@ -0,0 +1,9 @@
+import { Default2DComputePipeLine } from "./default_2d_compute_pipeline";
+import { Default3DPipeLine } from "./default_3d_pipeline";
+
+const registered_pipeline_types = {
+  Default2DComputePipeLine,
+  Default3DPipeLine,
+};
+
+export { registered_pipeline_types };
