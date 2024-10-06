24209f60d03df8141e7fec04541b2a91a2c22ffd
haroonsyed <haroonaminsyed@gmail.com>
2024-10-06 17:42:45 -0400

    Update build

--- dist/system/event_system.d.ts
+++ dist/system/event_system.d.ts
@@ -1,10 +1,9 @@
-import { EventEnum } from "./event_enums";
 type EventCallBack = (data: any) => Promise<void>;
 declare class GameEventSystem {
     private events;
     constructor();
-    subscribe(event_name: EventEnum, callback: EventCallBack): EventCallBack;
-    unsubscribe(event_name: EventEnum, callback: EventCallBack): void;
-    publish(event_name: EventEnum, data?: any): Promise<void>;
+    subscribe(event_name: string, callback: EventCallBack): EventCallBack;
+    unsubscribe(event_name: string, callback: EventCallBack): void;
+    publish(event_name: string, data?: any): Promise<void>;
 }
 export { GameEventSystem, EventCallBack };
